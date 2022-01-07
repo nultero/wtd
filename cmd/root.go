daa06840931469ee8a33b152d132c288a7d4aa96
nultero <nultero@protonmail.com>
2022-01-06 22:50:37 -0600

    added option to only use 1 goroutine

--- cmd/root.go
+++ cmd/root.go
@@ -11,6 +11,7 @@ import (
 
 var Debug bool = false
 var Depth int = 3
+var NoConcurrency bool = false
 var NoStrip bool = false
 var PrintAnyway bool = false
 var Reverse bool = false
@@ -40,7 +41,14 @@ var rootCmd = &cobra.Command{
 
 	Run: func(cmd *cobra.Command, args []string) {
 
-		lines := search.Default(&Verbose, &NoStrip, &Debug, &PrintAnyway)
+		var lines []string
+
+		if NoConcurrency {
+			lines = search.NoConc(&Verbose, &NoStrip, &Debug, &PrintAnyway)
+
+		} else {
+			lines = search.Default(&Verbose, &NoStrip, &Debug, &PrintAnyway)
+		}
 
 		if len(lines) == 0 {
 			printQmark()
@@ -58,10 +66,12 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().BoolVarP(&Debug, "debug", "g", false, "strips split str, useful only if SortLines throws an error (extremely rare)")
+	rootCmd.Flags().BoolVarP(&NoConcurrency, "noconc", "x", false, "prevents wtf from using additional goroutines")
 	rootCmd.Flags().BoolVarP(&NoStrip, "nostrip", "n", false, "prevents whitespace / tabs from being stripped when combing lines (for verbose flag only)")
 	rootCmd.Flags().BoolVarP(&PrintAnyway, "print", "p", false, "prints items as the channel iterates over a directory -- for whatever reason, some repos don't play nice")
 	rootCmd.Flags().BoolVarP(&Reverse, "reverse", "r", false, "least-priority results are listed first")
 	rootCmd.Flags().BoolVarP(&Verbose, "verbose", "v", false, "prints line contents of given TODO (but not multiline comments)")
 	rootCmd.Flags().StringVarP(&SearchStr, "searchstr", "s", "TODO", "searches for this string instead of some other, but does not compute priority")
-	// -d for depth?
+	// TODO -d for depth?
+	// TODO -i for ignore mapping some regex on filenames?
 }
---
+++ cmd/search/noconc.go
@@ -0,0 +1,92 @@
+package search
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"wtf/cmd/ignores"
+	"wtf/cmd/sorts"
+
+	"github.com/nultero/tics"
+)
+
+func NoConc(verbose, nostrip, debug, printAnyway *bool) []string {
+	here, err := os.Getwd()
+	if err != nil {
+		tics.ThrowSys(Default, err)
+	}
+
+	im := ignores.BasicIgMap()
+
+	lines := []string{}
+	baseSearch(here, &lines, &im, verbose, nostrip, debug)
+
+	for l := range lines {
+		if *printAnyway {
+			fmt.Println(l)
+		}
+	}
+
+	return lines
+}
+
+func baseSearch(dir string, lines *[]string, ignoreMap *ignores.Ignore, verbose, nostrip, debug *bool) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		tics.ThrowSys(search, err)
+	}
+
+	for _, f := range files {
+
+		if ignoreMap.NotExcluded(f.Name()) {
+
+			if f.IsDir() {
+				baseSearch(dir+"/"+f.Name(), lines, ignoreMap, verbose, nostrip, debug)
+
+			} else {
+				readFileNoConc(dir, f.Name(), lines, verbose, nostrip, debug)
+			}
+		}
+	}
+}
+
+func readFileNoConc(dir, fname string, lines *[]string, verbose, nostrip, debug *bool) {
+	b, err := os.ReadFile(dir + "/" + fname)
+	if err != nil {
+		tics.ThrowSys(readFile, err)
+	}
+
+	cont := string(b)
+	fileLines := strings.Split(cont, "\n")
+
+	for i, ln := range fileLines {
+		if strings.Contains(ln, "TODO") {
+
+			s := fmt.Sprintf(
+				"<%v> line: %v, ",
+				tics.Make(fname).DarkBlue().String(),
+				i+1, // ln #
+			)
+
+			priorityOfOs := countOs(ln)
+
+			if *verbose {
+				if *nostrip {
+					s += fmt.Sprintf("`%v`, ", ln)
+
+				} else {
+					ln = strings.TrimLeft(ln, " \t")
+					s += fmt.Sprintf("`%v`, ", ln)
+				}
+			}
+
+			if *debug {
+				s = strings.ReplaceAll(s, sorts.LineSplitStr, "")
+			}
+
+			s += fmt.Sprintf("%v%v", sorts.LineSplitStr, priorityOfOs)
+			*lines = append(*lines, s)
+		}
+	}
+}
