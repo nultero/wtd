a03f3512db9225bf141f33fd7b02de126cb8bf93
nultero <nultero@protonmail.com>
2022-03-01 22:56:47 -0600

    ugly code prototype of finding gitignore

--- src/dir_search.rs
+++ src/dir_search.rs
@@ -1,16 +1,30 @@
-// use dir;
+use std::{
+    fs::{ReadDir},
+    io::Error,
+};
 
-pub fn search() {
-    let cwd = std::env::current_dir();
-    let cwd = cwd.unwrap();
+// use crate::ignore::*;
 
-    let dir = cwd.read_dir();
-    let dir = dir.unwrap();
+// pub fn search(dir: ReadDir) {
+//     for file in dir {
+//         let file = file.unwrap();
 
-    for fl in dir {
-        println!("{:?}", fl);
-    }
+//         check_against_igs(file);
 
-    let cwd = cwd.to_str().unwrap();
-    println!("{}", cwd);
+//         if file.path().is_dir() {
+//             let nxt_dir = file.path().read_dir().unwrap();
+//             search(nxt_dir);
+//         } else {
+//             println!("plunk: {}", search_file(file));
+//         }
+//     }
+// }
+
+pub fn get_dir() -> Result<ReadDir, Error> {
+    let cwd = std::env::current_dir()?;
+    return cwd.read_dir();
 }
+
+// fn search_file(file: DirEntry) -> String {
+//     return file.path().to_str().unwrap().to_owned();
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use std::env;
 mod dir_search;
+mod ignore;
+mod launch;
+
+// use launch;
 
 fn main() {
-
     let args = env::args().skip(1);
     let len = args.len();
 
     if len == 0 {
-        dir_search::search();
-
+        launch::find_todos();
     } else if len == 1 {
         println!("placeholder");
-        
     } else {
         println!("placeholder");
     }
